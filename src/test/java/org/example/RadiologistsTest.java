26a2205609aada7d21f127059d3b696bfb0f6ce6
JackAnati <Anatijack0@gmail.com>
2020-03-03 20:20:51 +0200

    four submission

---
+++ src/test/java/org/example/RadiologistsTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+
+public class RadiologistsTest {
+
+
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void xRay() {
+
+    }
+}
